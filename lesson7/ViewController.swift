93cc7ba18a9f70ec53baf566d490d810442402d9
Iskander Nizamov <nizamov-iskander@yandex.ru>
2020-10-01 15:26:40 +0300

    homework7

--- lesson7/ViewController.swift
+++ lesson7/ViewController.swift
@@ -5,6 +5,44 @@
 //  Created by macbook on 01.10.2020.
 //  Copyright © 2020 Iskander Nizamov. All rights reserved.
 //
+enum UnviversityCalculatingError: Error {
+    
+    case divideByZero
+    case moreThanFive
+}
+
+class Student {
+    let name: String
+    let score: Int
+    
+    init(name: String, score: Int) {
+        self.name = name
+        self.score = score
+    }
+}
+
+class University {
+    var students = [Student]()
+    
+    var averageScore = 0
+    func averageScores() throws -> Int {
+        guard students.count > 0 else { throw UnviversityCalculatingError.divideByZero}
+        for student in students {
+            averageScore += student.score
+        }
+        averageScore = averageScore / students.count
+        guard averageScore > 5 else { throw UnviversityCalculatingError.moreThanFive}
+        return (averageScore)
+    }
+    
+    init(_ students: [Student]) {
+        self.students = students
+    }
+}
+
+
+
+
 
 import UIKit
 
@@ -13,6 +51,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let university1 = University([])
+        do { try print (university1.averageScores())}
+        catch UnviversityCalculatingError.divideByZero {
+            print ("Студенты еще не поступили в университет")
+        }
+        catch UnviversityCalculatingError.moreThanFive {
+            print ("Кто-то из студентов получает 6-ки")
+        }
+        catch _ {
+            print ("Произошло что то совсем невообразимое")
+        }
     }
 
 
